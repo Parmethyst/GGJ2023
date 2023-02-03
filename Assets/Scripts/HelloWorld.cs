dbdd64ffa1c740cc4ead3aff176262c315f565be
haruza123 <haruzakatana@gmail.com>
2023-02-03 17:56:18 +0700

    berubah

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //
+    
         //Hello
         Debug.Log("Hello world!");
     }
