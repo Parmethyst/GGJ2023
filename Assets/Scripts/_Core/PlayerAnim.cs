73276ba25a31db032d64cdcafc6b74f3cbb68e2c
Parmethyst <syncdrome84@gmail.com>
2023-02-04 14:07:10 +0700

    Animation working

--- Assets/Scripts/_Core/PlayerAnim.cs
+++ Assets/Scripts/_Core/PlayerAnim.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     private const string IS_WALKING = "isWalking";
+    private const string IS_START_JUMPING ="isStartJumping";
+    private const string IS_JUMPING="isJumping";
+    private const string IS_FINISHED_JUMPING="isFinishedJumping";
     [SerializeField] SpriteRenderer spriteRenderer;
     [SerializeField] GameInput input;
     [SerializeField] Animator animator;
+    [SerializeField] PlayerControl player;
     private bool isLastDirRight=true;
+    private bool isStartJumping=false;
+    private bool isJumping=false;
+    private bool isFinishedJumping=false;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void OnEnable() {
+        input.OnJumpInput += StartJump;
+    }
+    void OnDisable() {
+        input.OnJumpInput -= StartJump;
     }
 
     // Update is called once per frame
@@ -26,5 +35,17 @@ public class PlayerAnim : MonoBehaviour
             animator.SetBool(IS_WALKING, true);
         }
         else animator.SetBool(IS_WALKING, false);
+        if(animator.GetBool(IS_JUMPING) && player.IsGrounded()) {
+            animator.SetTrigger(IS_FINISHED_JUMPING);
+            animator.SetBool(IS_JUMPING,false);
+            animator.SetBool(IS_START_JUMPING,false);
+        }
+    }
+    public void StartJump() {
+        if(player.IsGrounded() && !animator.GetBool(IS_START_JUMPING)) {
+            animator.SetBool(IS_START_JUMPING,true);
+            
+        }
+        animator.SetBool(IS_JUMPING,true);
     }
 }
